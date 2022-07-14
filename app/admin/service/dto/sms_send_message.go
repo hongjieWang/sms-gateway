db13961e29e160c413d77ea066d2bbfbde76b0ef
小猿 <julywhj@163.com>
2022-07-14 14:53:06 +0800

    去除go

--- app/admin/service/dto/sms_send_message.go
+++ app/admin/service/dto/sms_send_message.go
@@ -2,8 +2,8 @@ package dto
 
 // SmsSendMessageReq 发送短信请求对象
 type SmsSendMessageReq struct {
-	BusinessNo string   `json:"businessNo"  comment:"服务编号"`
-	TemplateNo string   `json:"templateNo" comment:"模版编号"`
-	Phones     []string `json:"phones" comment:"手机号集合"`
-	Params     []string `json:"params" comment:"参数列表"`
+	BusinessNo string   `json:"businessNo" form:"businessNo" comment:"服务编号"`
+	TemplateNo string   `json:"templateNo" form:"templateNo" comment:"模版编号"`
+	Phones     []string `json:"phones" form:"phones" comment:"手机号集合"`
+	Params     []string `json:"params" form:"params" comment:"参数列表"`
 }
