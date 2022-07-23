4fec3716be077930fb13cba041af60ac7fe7dba9
小猿 <julywhj@163.com>
2022-07-24 00:01:14 +0800

    优化短信网关代码

--- app/admin/apis/sms_dashboard.go
+++ app/admin/apis/sms_dashboard.go
@@ -10,7 +10,7 @@ type SmsDashboard struct {
 	api.Api
 }
 
-func (e SmsDashboard) Get(c *gin.Context) {
+func (e SmsDashboard) GetAllCount(c *gin.Context) {
 	s := service.SmsDashboard{}
 	err := e.MakeContext(c).
 		MakeOrm().
@@ -22,5 +22,35 @@ func (e SmsDashboard) Get(c *gin.Context) {
 		return
 	}
 	count := s.GetAllCount()
+	e.OK(count, "查询成功")
+}
+
+func (e SmsDashboard) GetSuccessCount(c *gin.Context) {
+	s := service.SmsDashboard{}
+	err := e.MakeContext(c).
+		MakeOrm().
+		MakeService(&s.Service).
+		Errors
+	if err != nil {
+		e.Logger.Error(err)
+		e.Error(500, err, err.Error())
+		return
+	}
+	count := s.GetSuccessCount()
+	e.OK(count, "查询成功")
+}
+
+func (e SmsDashboard) GetRequestCount(c *gin.Context) {
+	s := service.SmsDashboard{}
+	err := e.MakeContext(c).
+		MakeOrm().
+		MakeService(&s.Service).
+		Errors
+	if err != nil {
+		e.Logger.Error(err)
+		e.Error(500, err, err.Error())
+		return
+	}
+	count := s.GetRequestCount()
 	e.OK(count, "查询成功")
 }
--- app/admin/router/sms_dashboard.go
+++ app/admin/router/sms_dashboard.go
@@ -1,1 +1,23 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	jwt "github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth"
+	"go-admin/app/admin/apis"
+	"go-admin/common/middleware"
+)
+
+func init() {
+	routerCheckRole = append(routerCheckRole, smsDashboardRouter)
+}
+
+// registerSmsSendLogRouter
+func smsDashboardRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
+	api := apis.SmsDashboard{}
+	r := v1.Group("/sms-dashboard").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole())
+	{
+		r.GET("/count", api.GetAllCount)
+		r.GET("/get-success-count", api.GetSuccessCount)
+		r.GET("/get-request-count", api.GetRequestCount)
+	}
+}
