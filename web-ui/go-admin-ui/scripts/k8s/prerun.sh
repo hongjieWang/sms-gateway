e502ae0d720000d6f5eb80c7037865920a27e2ef
小猿 <julywhj@163.com>
2022-07-27 11:30:46 +0800

    移除web

--- web-ui/go-admin-ui/scripts/k8s/prerun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-kubectl create ns go-admin
-kubectl create configmap nginx-frontend --from-file=./default.conf -n go-admin
--- web-ui/go-admin-ui/src/api/admin/sms-send-message.js
+++
@@ -1,9 +0,0 @@
-import request from "@/utils/request";
-// 新增smsSendMessage
-export function smsSendMessage(data) {
-  return request({
-    url: "/api/v1/send-message",
-    method: "post",
-    data: data,
-  });
-}
